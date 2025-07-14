a9ade1bcdc4f58c22c9fa9b54991ccf9905860bd
yosra <yosraberrouayel@gmail.com>
2025-07-14 22:45:35 +0200

    updated package

--- R/ChIPDBData.R
+++ R/ChIPDBData.R
@@ -28,31 +28,36 @@ getChIPDB <- function(name = c(
 )) {
   name <- match.arg(name)
 
-  # Mapping from user-friendly names to EH IDs
-  eh_map <- list(
-    ENCODE_rE2G          = "EH9847",
-    ENCODE_rE2G_25score  = "EH9848",
-    ENCODE_rE2G_50score  = "EH9849",
-    ENCODE_rE2G_75score  = "EH9850",
-    ENCODE_rE2G_50depth  = "EH9851",
-    ENCODE_rE2G_100depth = "EH9852",
-    ENCODE_rE2G_200depth = "EH9853",
-    ENCODE_rE2G_300depth = "EH9854",
-    CREdb                = "EH9855",
-    GeneHancer           = "EH9856"
+  file_map <- list(
+    ENCODE_rE2G           = "ChIPDBData/records/15667485/files/encode_re2g_object_hg38.rda",
+    ENCODE_rE2G_25score   = "ChIPDBData/records/15667485/files/encode_re2g-25_object_hg38.rda",
+    ENCODE_rE2G_50score   = "ChIPDBData/records/15667485/files/encode_re2g-5_object_hg38.rda",
+    ENCODE_rE2G_75score   = "ChIPDBData/records/15667485/files/encode_re2g-75_object_hg38.rda",
+    ENCODE_rE2G_50depth   = "ChIPDBData/records/15667485/files/encode_re2g-50depth_object_hg38.rda",
+    ENCODE_rE2G_100depth  = "ChIPDBData/records/15667485/files/encode_re2g-100depth_object_hg38.rda",
+    ENCODE_rE2G_200depth  = "ChIPDBData/records/15667485/files/encode_re2g-200depth_object_hg38.rda",
+    ENCODE_rE2G_300depth  = "ChIPDBData/records/15667485/files/encode_re2g-300depth_object_hg38.rda",
+    CREdb                 = "ChIPDBData/records/15667485/files/credb_object_hg38.rda",
+    GeneHancer            = "ChIPDBData/records/15667485/files/genehancer_object_hg38.rda"
   )
 
-  eh_id <- eh_map[[name]]
+  eh <- ExperimentHub::ExperimentHub()
+  resource <- file_map[[name]]
 
-  if (is.null(eh_id)) {
+  if (is.null(resource)) {
     stop("Invalid dataset name. See ?getChIPDB for allowed options.")
   }
 
-  eh <- ExperimentHub::ExperimentHub()
-
   tryCatch({
-    obj <- eh[[eh_id]]
-    return(obj)
+    path <- eh[[resource]]
+    env <- new.env()
+    loaded_names <- load(path, envir = env)
+
+    if (length(loaded_names) != 1) {
+      warning("More than one object was loaded; returning the first one: ", loaded_names[1])
+    }
+
+    get(loaded_names[1], envir = env)
   }, error = function(e) {
     stop("Failed to retrieve the dataset from ExperimentHub. Please ensure your internet connection is active and that the dataset exists.\n\n", conditionMessage(e))
   })
